e35c74f58b530061d69705d4f588d4a5dfb9fb5c
nasty <nastyushas18.93@gmail.com>
2026-08-05 21:02:25 +0300

    Test2

---
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -0,0 +1,31 @@
+package ui_tests;
+
+import dto.UserLombok;
+import manager.AppManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+import java.util.Random;
+
+public class RegistrationTests extends AppManager
+{
+    @BeforeMethod
+    public void goToRegistrationPage()
+    {
+        new HomePage(getDriver()).clickBtnLogin();
+    }
+    @Test
+    public void registrationPositiveTest()
+    {
+        int i = new Random().nextInt(1000);
+        UserLombok user = UserLombok.builder()
+                .username("ghdjd" + i + "nbd@hghg.bh")
+                .password("Adfeyt23!")
+                .build();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+    }
+}
