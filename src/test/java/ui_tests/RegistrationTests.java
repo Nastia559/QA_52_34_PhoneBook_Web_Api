3983bf31ab2fd0384f5b424547c0b57d4cd51bec
nasty <nastyushas18.93@gmail.com>
2026-08-09 21:05:49 +0300

    Test2

--- src/test/java/ui_tests/RegistrationTests.java
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -2,17 +2,22 @@ package ui_tests;
 
 import dto.UserLombok;
 import manager.AppManager;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.ContactsPage;
 import pages.HomePage;
 import pages.LoginPage;
+import static utils.UserFactory.*;
 
 import java.util.Random;
 
 public class RegistrationTests extends AppManager {
+    LoginPage loginPage;
     @BeforeMethod
     public void goToRegistrationPage() {
         new HomePage(getDriver()).clickBtnLogin();
+        loginPage = new LoginPage(getDriver());
     }
 
     @Test
@@ -22,13 +27,35 @@ public class RegistrationTests extends AppManager {
                 .username("ghdjd" + i + "nbd@hghg.bh")
                 .password("Adfeyt23!")
                 .build();
-        LoginPage loginPage = new LoginPage(getDriver());
         loginPage.typeLoginRegistrationForm(user);
         loginPage.clickBtnRegistration();
+        Assert.assertTrue(new ContactsPage(getDriver())
+                .validateTextInMessageNoContacts("No Contacts here!"));
     }
 
     @Test
-    public void testMethod() {
-        new HomePage(getDriver()).ajaxMethod();
+    public void registrationPositiveWithFakerTest() {
+    //UserLombok user = UserFactory.positiveUser();
+        UserLombok user = positiveUser();
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+        Assert.assertTrue(new ContactsPage(getDriver())
+                .validateTextInMessageNoContacts("No Contacts here!"));
     }
+
+    @Test
+    public void registrationNegativeEmptyAllFieldsTest(){
+        loginPage.clickBtnRegistration();
+        Assert.assertTrue(loginPage.closeAlert()
+                .contains("Wrong email or password format"));
+    }
+//    @Test
+//    public void testMethod() {
+//        new HomePage(getDriver()).method();
+//    }
+//
+//    @Test
+//    public void testAjaxMethod() {
+//        new HomePage(getDriver()).ajaxMethod();
+//    }
 }
